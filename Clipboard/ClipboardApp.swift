c131fb14d184d5e609ced09f561be7cbe2a1c861
Dhanilka <dhanilkaid@gmail.com>
2026-03-08 22:56:19 +0530

    Initial Commit

---
+++ Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by Dhanilka Dasanayake on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
